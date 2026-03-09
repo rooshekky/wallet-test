6d19dca88bb0ba3f819bb6fd6d127499f473f660
rooshekky <rooshekky@gmail.com>
2026-03-08 18:24:05 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+import os
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"status": "running"}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
