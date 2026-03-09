185a85491aad3e8e5d01fca8f9ce0f2149f27ba6
rooshekky <rooshekky@gmail.com>
2026-03-08 18:26:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
 import os
 import uvicorn
+from eth_account import Account
 
 app = FastAPI()
 
 @app.get("/")
-def root():
+def home():
     return {"status": "running"}
+
+@app.get("/wallet/create")
+def create_wallet():
+    acct = Account.create()
+
+    return {
+        "address": acct.address,
+        "private_key": acct.key.hex()
+    }
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
